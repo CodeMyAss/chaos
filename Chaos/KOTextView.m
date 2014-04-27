e123889f155a525a5c1325154a134f859c7583b7
Steven Degutis <steven@cleancoders.com>
2014-04-27 18:15:36 -0500

    much more promising!

--- Chaos/KOTextView.m
+++ Chaos/KOTextView.m
@@ -79,3 +79,81 @@
 }
 
 @end
+
+
+
+
+
+
+
+#import <QuartzCore/QuartzCore.h>
+
+@interface SDTextLayer : CATextLayer
+@end
+
+@implementation SDTextLayer
+
+- (void) drawInContext:(CGContextRef)ctx {
+    CGContextSetFillColorWithColor(ctx, self.backgroundColor);
+    CGContextFillRect(ctx, [self bounds]);
+    CGContextSetShouldSmoothFonts(ctx, true);
+    
+    [super drawInContext:ctx];
+}
+
+@end
+
+
+@interface SDView : NSView
+@end
+
+@implementation SDView
+
+- (id) initWithFrame:(NSRect)frameRect {
+    if (self = [super initWithFrame:frameRect]) {
+        [self setLayer:[CALayer layer]];
+        [self setWantsLayer:YES];
+    }
+    return self;
+}
+
+- (void) awakeFromNib {
+    [super awakeFromNib];
+    
+    int c = 'a';
+    
+    NSRect r = [@"x" boundingRectWithSize:NSMakeSize(200, 200) options:0 attributes:@{NSFontAttributeName: [NSFont fontWithName:@"Menlo" size:12]}];
+    
+    int w = (r.size.width + r.origin.x);
+    int h = (r.size.height + r.origin.y);
+    
+    NSLog(@"%d, %d", w, h);
+    
+    //    w = r.size.width + r.origin.x;
+    //    h = r.size.height + r.origin.y;
+    
+    for (int y = 0; y < 10; y++) {
+        for (int x = 0; x < 30; x++) {
+            CATextLayer* l = [SDTextLayer layer];
+            
+            l.frame = NSMakeRect(x * w, y * h, w, h);
+            l.font = (__bridge CFTypeRef)[NSFont fontWithName:@"Menlo" size:12];
+            l.fontSize = 12;
+            l.foregroundColor = [NSColor blackColor].CGColor;
+            l.backgroundColor = [NSColor whiteColor].CGColor;
+            
+            l.backgroundColor = (rand() % 10 > 5 ? [NSColor yellowColor] : [NSColor whiteColor]).CGColor;
+            
+            //            l.foregroundColor = [NSColor yellowColor].CGColor;
+            //            l.backgroundColor = [NSColor blueColor].CGColor;
+            l.string = [NSString stringWithFormat:@"%c", c];
+            [[self layer] addSublayer:l];
+            
+            //            if (c == '_') c = 'a' - 1;
+            if (++c == 'z' + 1) c = 'a';
+        }
+    }
+    
+}
+
+@end
