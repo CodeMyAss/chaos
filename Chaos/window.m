3456486ef0a47ac7c5fe307e1ea8b4fca5e1d105
Steven Degutis <steven@cleancoders.com>
2014-05-05 12:47:54 -0500

    fix

--- Chaos/window.m
+++ Chaos/window.m
@@ -1,24 +1,23 @@
 #import "lua/lauxlib.h"
 #import "KOWindowController.h"
 
-static NSMutableDictionary* colors;
-
 static NSColor* SDColorFromHex(const char* hex) {
+    static NSMutableDictionary* colors;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         colors = [NSMutableDictionary dictionary];
     });
     
-    NSString* hexString = [NSString stringWithUTF8String: hex];
+    NSString* hexString = [[NSString stringWithUTF8String: hex] uppercaseString];
     NSColor* color = [colors objectForKey:hexString];
     
     if (!color) {
         NSScanner* scanner = [NSScanner scannerWithString: hexString];
         unsigned colorCode = 0;
         [scanner scanHexInt:&colorCode];
-        color = [NSColor colorWithCalibratedRed: (colorCode >> 16) & 0xFF / 0xFF
-                                          green: (colorCode >> 8) & 0xFF / 0xFF
-                                           blue: (colorCode) & 0xFF / 0xFF
+        color = [NSColor colorWithCalibratedRed:(CGFloat)(unsigned char)(colorCode >> 16) / 0xff
+                                          green:(CGFloat)(unsigned char)(colorCode >> 8) / 0xff
+                                           blue:(CGFloat)(unsigned char)(colorCode) / 0xff
                                           alpha: 1.0];
         [colors setObject:color forKey:hexString];
     }
